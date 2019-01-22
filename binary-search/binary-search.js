53204b2872990d5c201fbce1adf9aab7f475fda4
jon cannon <iamjoncannon@gmail.com>
2019-01-22 17:46:12 -0500

    ooo

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,31 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
+// const binarySearch = (array, target) => {
 	
-};
+// 	let middle = Math.floor(array.length / 2);
+// 	let middleNode = array[middle]
+// 	let beginning = array[0]
+// 	let end = array[array.length -1]
+
+// 	if (beginning === end){
+// 		return false 
+// 	}
+// 	else if (target === beginning || target === middleNode || target === end){
+// 		return true
+// 	}
+
+// 	else if (target > middleNode){
+// 		return binarySearch(array.slice(middle), target)
+// 	}
+
+// 	else if(target < middleNode){
+// 		return binarySearch(array.slice(0, middle), target)
+// 	}
+
+// 	return false
+
+// };
 
 /*
 	EXTRA CREDIT:
@@ -13,5 +35,39 @@ const binarySearch = (array, target) => {
 	How can we use other pieces of data, like pointers, to avoid slicing?
 
 */
+
+const binarySearch = (array, target) => {
+	
+	let middleVal; 
+	let beginningVal = 0
+	let endVal = array.length -1
+
+	while( array[beginningVal] !== array[endVal] ) {
+
+		console.log(middleVal, beginningVal, endVal)
+
+		middleVal = Math.floor( (endVal - beginningVal) / 2)
+
+		if (target === array[beginningVal] || 
+			target === array[middleVal] || 
+			target === array[endVal]
+			){
+			return true
+		}
+
+		else if (target > array[middleVal]){
+			beginningVal = middleVal
+		}
+
+		else if(target < array[middleVal]){
+			endVal = middleVal
+		}
+
+
+	}
+
+	return false
+
+};
 
 module.exports = binarySearch
--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,31 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+	const cache = [linkedlist.head];
+	
+	let thisNode = linkedlist.head
 
+	while(thisNode.next){
+		if(cache.includes(thisNode.next.value)){
+			return true  // thisNode.value  // || true //depending on extra credit or not;
+		} else {
+			cache.push(thisNode.next.value);
+			thisNode = thisNode.next;
+		}
+	}
+	return false;
 };
 
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+// const letters = ['a','b','c','d','e','f','g','h','i'];
+// const emojis = ['🥚','🐣','🦅 ', '👾'];
+
+
+// const test1 = new LinkedList();
+// test1.addToHead(0)
+// numbers.forEach(val => test1.addToTail(val));
+// test1.getNthNode(6).next = test1.getNthNode(3);
 
 /*
 EXTRA CREDIT:
@@ -12,6 +34,9 @@ EXTRA CREDIT:
 Write a function findLoop() that consumes a linkedlist with a loop
 This function should return the Node value the loop begins at
 Remember to write some test specs too!
+//cache is object of current value : current next value
+//cache.HasOwnProperty(currentNextValue)
 
 */
+
 module.exports = isLoop
